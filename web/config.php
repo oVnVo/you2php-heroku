4194d39178815ae5d0f74f3472407213dd72b276
oVnVo <a13971522032@163.com>
2018-09-12 17:29:47 +0800

    Update config.php

--- web/config.php
+++ web/config.php
@@ -1,7 +1,8 @@
 <?php
+$gl=(isset($_COOKIE['gl']) && $_COOKIE['gl'])?$_COOKIE['gl']:'HK';
 define('ROOT_PART', Root_part());
 define('APIKEY', 'AIzaSyBqBDz6-Byy5XQ4msY0UOocNPGTfFB_M1E');//这里改成您的api key
-define('GJ_CODE', 'HK');//国家代码
+define('GJ_CODE', $gl);
 define('SITE_NAME', 'Y');//网站名字
 define('TITLENAME', 'Y');//网站名字
 define('EN2DEKEY', 'A&KU$FR#%E&');//一个随机字符串，加解密KEY
